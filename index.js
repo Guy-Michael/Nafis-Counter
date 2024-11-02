6e5da62d39b098f2dcc0a62824b2bd34f90a30f8
Guy Michael <guy.michael275@gmail.com>
2024-11-02 18:01:25 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+
+	const base = 'מספר הימים שענבר סירבה לבוא איתי לנאפיס:';
+	
+	const baseDate = new Date("11/02/2024");
+	const dateDiff = Date.now() - baseDate;
+	const diffTime = Math.abs(Date.now() - baseDate);
+	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+	
+	const fullMessage = `${base} ${diffDays}`;
+	console.log(fullMessage);
+	
+	const p = document.getElementById("message");
+	p.textContent = fullMessage;
+	console.log(p);
+};
