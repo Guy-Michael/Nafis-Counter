1884c22db49d6204703a987bd4ab7c00a8612521
Guy Michael <guy.michael275@gmail.com>
2024-11-02 18:06:05 +0200

    fix message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = () => {
 
-	const base = 'מספר הימים שענבר סירבה לבוא איתי לנאפיס:';
+	const base = ' מספר הימים שענבר סירבה לבוא איתי לנאפיס מאז ה2.11:';
 	
 	const baseDate = new Date("11/02/2024");
 	const dateDiff = Date.now() - baseDate;
