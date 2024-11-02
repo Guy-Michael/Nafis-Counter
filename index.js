81aa5b96ddbb80e9b5b3a4da09cb53b2754ec99c
Guy Michael <guy.michael275@gmail.com>
2024-11-02 18:27:20 +0200

    add date list

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 window.onload = () => {
 
-	const base = ' מספר הימים שענבר סירבה לבוא איתי לנאפיס מאז ה2.11:';
+	const base = ' מספר הימים שענבר סירבה לבוא איתי לנאפיס:';
 	
 	const baseDate = new Date("11/02/2024");
-	const dateDiff = Date.now() - baseDate;
 	const diffTime = Math.abs(Date.now() - baseDate);
 	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
 	
 	const fullMessage = `${base} ${diffDays}`;
-	console.log(fullMessage);
 	
 	const p = document.getElementById("message");
 	p.textContent = fullMessage;
-	console.log(p);
+	const array = getDaysArray(baseDate, Date.now());
+	
+	const datesDiv = document.getElementById('dates');
+	
+	for(let i = 0; i < array.length; i++) {
+		console.log(array[i]);
+		const dateDiv = document.createElement("div");
+		const content =  new Date(array[i]).toLocaleDateString('heb-IL') + ': ❌';
+		dateDiv.textContent = content
+		datesDiv.appendChild(dateDiv);
+	}
+};
+
+const getDaysArray = function(start, end) {
+    const arr = [];
+    for(const dt=new Date(start); dt<=new Date(end); dt.setDate(dt.getDate()+1)){
+        arr.push(new Date(dt));
+    }
+    return arr;
 };
